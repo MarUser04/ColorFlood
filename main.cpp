68031cda674dd2728b4834ca4bada22384f8d860
marco <usechermarcoj@gmail.com>
2016-04-26 17:36:46 -0430

    agregue menu y arregle, a medias, la interfaz

--- main.cpp
+++ main.cpp
@@ -12,15 +12,105 @@ struct tablero{
 
 void cargarmatriz(tablero *tm);
 void color(int X);
+void gotoxy(int x,int y);
+void menu();
+void seleccion();
 
 int main(int argc, char** argv) {
-	
+	srand(time(0));
 	system("color f0");
 	
 	tablero tm;
-	cargarmatriz(&tm);
+
+	char tecla;
+	int band=0, band1=0, n;
+	menu();
 	
-	srand(time(0));
+	
+	do{	
+	
+		tecla=getch( );
+		switch(tecla){
+			case 49:{
+				
+				system("cls");
+				band=1;
+				break;
+			}
+			case 50:{
+	          
+	          system("cls");
+	         
+	          do{
+	          	printf("\n\nSI DESEA REGRESAR AL MENU PRINCIPAL PRESIONE <4>.");
+	            scanf("%d",&n);
+	            
+	             if(n!=4){
+	          	     printf("\n OPCION INVALIDA, INTENTE DE NUEVO");
+				  }
+	          	
+	          	
+			  }while(n!=4);
+	          
+	          if(n==4){
+	          	
+	          	band1=1;
+	          	system("cls");
+			  }
+				
+				break;
+			}
+			case 51:{
+				system("cls");
+				printf("CREADOR: MARCO JOSE USECHE RIVERA\n ESTUDIANTE INGENIERIA EN INFORMATICA\n UNET 216 ");
+				do{
+	          	  printf("\n\nSI DESEA REGRESAR AL MENU PRINCIPAL PRESIONE <4>.");
+	            	scanf("%d",&n);
+	          	  if(n!=4){
+	          	     printf("\n OPCION INVALIDA, INTENTE DE NUEVO");
+				  }
+				  
+	          	
+			   }while(n!=4);
+	          
+	           if(n==4){
+	          	
+	          	band1=1;
+	          	system("cls");
+			   }
+				break;
+			}
+			default:{
+			
+				
+				gotoxy(26,8);
+				printf("\n OPCION INVALIDA, INTENTE DE NUEVO");
+				
+				break;
+			}
+			
+		}//switch
+		
+		 if(band1==1){
+	    	
+	       menu( );
+	   
+	     }
+		
+		
+		
+	}while(band!=1);
+	
+	if(band==1){
+		system("cls");
+			tablero tm;
+        	cargarmatriz(&tm);
+	
+	}
+	
+	
+
+	
 	
 	system("pause>null");
 	return 0;
@@ -30,16 +120,26 @@ void color(int X)
 { 
 SetConsoleTextAttribute(GetStdHandle (STD_OUTPUT_HANDLE),X); 
 } 
+
+void gotoxy(int x,int y){  
+      HANDLE hcon;  
+      hcon = GetStdHandle(STD_OUTPUT_HANDLE);  
+      COORD dwPos;  
+      dwPos.X = x;  
+      dwPos.Y= y;  
+      SetConsoleCursorPosition(hcon,dwPos);  
+ } 
+ 
 void cargarmatriz(tablero *tm)
 {
 	char opc; 
 	int aux;
 	do{
 	system("cls");
-	printf("-------BIENVENIDO A COLORFLOOD---USECHE EDITION V.1 CON BUGS---------\n");
-	printf("-------FACIL-----\n");
-	printf("-------MEDIO-----\n");
-	printf("-------DIFICEL---\n");
+	printf("-------SELECCIONE SU DIFICULTAD-------\n");
+	printf("-1-----FACIL-----\n");
+	printf("-2-----MEDIO-----\n");
+	printf("-3-----DIFICIL---\n");
 
     
     fflush(stdin);
@@ -64,7 +164,7 @@ void cargarmatriz(tablero *tm)
 			break;
 	
 	}
-	
+
 	tm->matriz= new int *[tm->filas];
 	for(int i=0; i<tm->filas; i++)
 	{
@@ -114,5 +214,18 @@ void cargarmatriz(tablero *tm)
 	}
 	
 	
+	
+}
+
+void menu()
+{
+	printf("\t-------BIENVENIDO A COLORFLOOD---USECHE EDITION V. 0.1 CON BUGS---------\n");
+	printf("\n\n\t-------(1)MODOS DE JUEGO----\n");
+	printf("\t-------(2)AYUDA-------------\n");
+	printf("\t-------(3)CREDITOS----------\n");
+}
+
+void seleccion()
+{
 	
 }
